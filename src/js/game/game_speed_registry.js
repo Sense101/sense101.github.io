17ca44a601371a7cb58917df94430219784cb122
Sense101 <67970865+Sense101@users.noreply.github.com>
2020-11-15 11:30:11 +0000

    hahahha

--- src/js/game/game_speed_registry.js
+++
@@ -1,8 +0,0 @@
-import { RegularGameSpeed } from "./time/regular_game_speed";
-import { gGameSpeedRegistry } from "../core/global_registries";
-
-export function initGameSpeedRegistry() {
-    gGameSpeedRegistry.register(RegularGameSpeed);
-
-    // Others are disabled for now
-}
