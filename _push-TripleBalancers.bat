512ec4825b130b9f3b6270d1fbb6e5f59c94ee42
Sense101 <67970865+Sense101@users.noreply.github.com>
2020-12-22 12:30:49 +0000

    k

--- _push-TripleBalancers.bat
+++ _push-TripleBalancers.bat
@@ -1,12 +1,2 @@
-robocopy "../shapez.io/src/" "./" "index.html"
-
-robocopy "../shapez.io/build/" "./shapez/TripleBalancers" /S
-
-git add -A
-
-git commit -m "_push-TripleBalancers"
 
 git push
-
-:x
-goto x
