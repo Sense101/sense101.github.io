4d97323eb6a1203adbfb1be408b07fd9de4bef93
Sense101 <67970865+Sense101@users.noreply.github.com>
2021-01-25 13:46:25 +0000

    _push-ShapezIndustries

--- shapez/ShapezIndustries/bundle.js
+++ shapez/ShapezIndustries/bundle.js
@@ -49724,15 +49724,17 @@ class HUDPinnedShapes extends _base_hud_part__WEBPACK_IMPORTED_MODULE_7__["BaseH
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "HUDSandboxController", function() { return HUDSandboxController; });
 /* harmony import */ var _core_utils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../core/utils */ "./src/js/core/utils.js");
-/* harmony import */ var _base_hud_part__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../base_hud_part */ "./src/js/game/hud/base_hud_part.js");
-/* harmony import */ var _dynamic_dom_attach__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../dynamic_dom_attach */ "./src/js/game/hud/dynamic_dom_attach.js");
-/* harmony import */ var _notifications__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./notifications */ "./src/js/game/hud/parts/notifications.js");
+/* harmony import */ var _tutorial_goals__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../tutorial_goals */ "./src/js/game/tutorial_goals.js");
+/* harmony import */ var _base_hud_part__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../base_hud_part */ "./src/js/game/hud/base_hud_part.js");
+/* harmony import */ var _dynamic_dom_attach__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../dynamic_dom_attach */ "./src/js/game/hud/dynamic_dom_attach.js");
+/* harmony import */ var _notifications__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./notifications */ "./src/js/game/hud/parts/notifications.js");
 
 
 
 
 
-class HUDSandboxController extends _base_hud_part__WEBPACK_IMPORTED_MODULE_1__["BaseHUDPart"] {
+
+class HUDSandboxController extends _base_hud_part__WEBPACK_IMPORTED_MODULE_2__["BaseHUDPart"] {
     createElements(parent) {
         this.element = Object(_core_utils__WEBPACK_IMPORTED_MODULE_0__["makeDiv"])(
             parent,
@@ -49773,18 +49775,14 @@ class HUDSandboxController extends _base_hud_part__WEBPACK_IMPORTED_MODULE_1__["
                     <button class="styledButton plus">+</button>
                 </div>
 
-                <div class="additionalOptions">
-                    <button class="styledButton giveBlueprints">Fill blueprint shapes</button>
-                    <button class="styledButton maxOutAll">Max out all</button>
-                </div>
             </div>
         `
         );
 
         const bind = (selector, handler) => this.trackClicks(this.element.querySelector(selector), handler);
 
-        bind(".giveBlueprints", this.giveBlueprints);
-        bind(".maxOutAll", this.maxOutAll);
+        //bind(".giveBlueprints", this.giveBlueprints);
+        //bind(".maxOutAll", this.maxOutAll);
         bind(".levelToggle .minus", () => this.modifyLevel(-1));
         bind(".levelToggle .plus", () => this.modifyLevel(1));
 
@@ -49819,10 +49817,12 @@ class HUDSandboxController extends _base_hud_part__WEBPACK_IMPORTED_MODULE_1__["
     modifyUpgrade(id, amount) {
         const upgradeTiers = this.root.gameMode.getUpgrades()[id];
         const maxLevel = upgradeTiers.length;
+        const maxResearchLevel = this.root.hubGoals.researchLevel;
 
+        const isAtMax = this.root.hubGoals.upgradeLevels[id] >= maxResearchLevel;
         this.root.hubGoals.upgradeLevels[id] = Math.max(
             0,
-            Math.min(maxLevel, (this.root.hubGoals.upgradeLevels[id] || 0) + amount)
+            Math.min(maxLevel, maxResearchLevel, (this.root.hubGoals.upgradeLevels[id] || 0) + amount),
         );
 
         // Compute improvement
@@ -49832,14 +49832,29 @@ class HUDSandboxController extends _base_hud_part__WEBPACK_IMPORTED_MODULE_1__["
         }
         this.root.hubGoals.upgradeImprovements[id] = improvement;
         this.root.signals.upgradePurchased.dispatch(id);
-        this.root.hud.signals.notification.dispatch(
-            "Upgrade '" + id + "' is now at tier " + (this.root.hubGoals.upgradeLevels[id] + 1),
-            _notifications__WEBPACK_IMPORTED_MODULE_3__["enumNotificationType"].upgrade
-        );
+        if(isAtMax) {
+            this.root.hud.signals.notification.dispatch(
+                "Upgrade '" + id + "' is already at the max tier unlocked",
+                _notifications__WEBPACK_IMPORTED_MODULE_4__["enumNotificationType"].upgrade
+            );
+        } else {
+            this.root.hud.signals.notification.dispatch(
+                "Upgrade '" + id + "' is now at tier " + (this.root.hubGoals.upgradeLevels[id] + 1),
+                _notifications__WEBPACK_IMPORTED_MODULE_4__["enumNotificationType"].upgrade
+            );
+        }
+        
     }
 
     modifyLevel(amount) {
         const hubGoals = this.root.hubGoals;
+        if(hubGoals.level + amount > 20) {
+            this.root.hud.signals.notification.dispatch(
+                "You can't cheat past level 20 :/",
+                _notifications__WEBPACK_IMPORTED_MODULE_4__["enumNotificationType"].upgrade
+            );
+            return;
+        }
         hubGoals.level = Math.max(1, hubGoals.level + amount);
         hubGoals.computeNextGoal();
 
@@ -49850,31 +49865,35 @@ class HUDSandboxController extends _base_hud_part__WEBPACK_IMPORTED_MODULE_1__["
 
         // Compute gained rewards
         hubGoals.gainedRewards = {};
+        hubGoals.researchLevel = 1;
         const levels = this.root.gameMode.getLevelDefinitions();
         for (let i = 0; i < hubGoals.level - 1; ++i) {
             if (i < levels.length) {
                 const reward = levels[i].reward;
+                if(reward == _tutorial_goals__WEBPACK_IMPORTED_MODULE_1__["enumHubGoalRewards"].reward_research_level) {
+                    hubGoals.researchLevel++;
+                }
                 hubGoals.gainedRewards[reward] = (hubGoals.gainedRewards[reward] || 0) + 1;
             }
         }
 
         this.root.hud.signals.notification.dispatch(
             "Changed level to " + hubGoals.level,
-            _notifications__WEBPACK_IMPORTED_MODULE_3__["enumNotificationType"].upgrade
+            _notifications__WEBPACK_IMPORTED_MODULE_4__["enumNotificationType"].upgrade
         );
     }
 
     initialize() {
         // Allow toggling the controller overlay
-        //this.root.gameState.inputReciever.keydown.add(key => {
-        //    if (key.keyCode === 117) {
-        //        // F6
-        //        this.toggle();
-        //    }
-        //});
+        this.root.gameState.inputReciever.keydown.add(key => {
+            if (key.keyCode === 117) {
+                // F6
+                this.toggle();
+            }
+        });
 
         this.visible = !true;
-        this.domAttach = new _dynamic_dom_attach__WEBPACK_IMPORTED_MODULE_2__["DynamicDomAttach"](this.root, this.element);
+        this.domAttach = new _dynamic_dom_attach__WEBPACK_IMPORTED_MODULE_3__["DynamicDomAttach"](this.root, this.element);
     }
 
     toggle() {
